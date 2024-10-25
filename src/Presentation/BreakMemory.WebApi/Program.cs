ea6ab620409aff0ebf1a02217f1cb926790c8c35
AlexanderLukashuk <upiter465@gmail.com>
2024-10-25 13:10:35 +0500

    register DI from Application

--- src/Presentation/BreakMemory.WebApi/Program.cs
+++ src/Presentation/BreakMemory.WebApi/Program.cs
@@ -1,9 +1,10 @@
-using BreakMemory.WebApi.Models;
+using BreakMemory.Application;
 using FastEndpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager appConfiguration = builder.Configuration;
 
-builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("BreakMemory"));
+builder.Services.AddApplication(appConfiguration);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
